6d4a794ca853bae56d969c29b4081d73fbb00cc9
NisGow <nisha.gowda@utexas.edu>
2020-08-13 20:26:28 +0000

    added  show feature

--- YelpCamp/app.js
+++ YelpCamp/app.js
@@ -1,30 +1,36 @@
 let express = require("express"),
  	app = express(),
- 	bodyParser = require("body-parser"),
-	mongoose = require("mongoose");
+ 	bodyParser = require("body-parser");
 
-mongoose.connect("mongodb://localhost/yelp_camp");
+let mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost:27017/yelp_camp', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+.then(() => console.log('Connected to DB!'))
+.catch(error => console.log(error.message));
 
 //Schema Setup
 let campgroundSchema = new mongoose.Schema({
 	name:String,
-	image: String
+	image: String,
+	description: String
+	
 });
 
 let Campground = mongoose.model("Campground", campgroundSchema);
+// Campground.create({
+// 	name: "Salmon Creek", 
+// 	image: " https://images.unsplash.com/photo-1478131143081-80f7f84ca84d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
+// 	description:"Huge granite hill"
+// }, function(err,campground){
+// 	if(!err){
+// 		console.log("New campground");
+// 		console.log(campground);
+// 	}
+// });
 
 
-
-let campgrounds = [
-		{name: "Salmon Creek", image: " https://images.unsplash.com/photo-1478131143081-80f7f84ca84d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"},
-		{name: "Granite Hill", image: " https://images.unsplash.com/photo-1508873696983-2dfd5898f08b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"},
-		{name: "Salmon Creek", image: " https://images.unsplash.com/photo-1571687949921-1306bfb24b72?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"},
-	{name: "Salmon Creek", image: " https://images.unsplash.com/photo-1478131143081-80f7f84ca84d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"},
-		{name: "Granite Hill", image: " https://images.unsplash.com/photo-1508873696983-2dfd5898f08b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"},
-		{name: "Salmon Creek", image: " https://images.unsplash.com/photo-1571687949921-1306bfb24b72?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"},
-		
-		
-	]
 app.use(bodyParser.urlencoded({extended:true}));
 
 app.set("view engine","ejs");
@@ -34,28 +40,55 @@ app.get("/",function(req,res){
 })
 
 //use copy image address
+//Index
 app.get("/campgrounds",function(req,res){
+	//Get all campgrounds from DB
+	Campground.find({}, function(err, allCampgrounds){
+		if(!err){
+			res.render("index", {campgrounds:allCampgrounds});
+		}
+	})
 	
-	res.render("campgrounds", {campgrounds:campgrounds});
 })
 
+//Create - add new campgrounds
 app.post("/campgrounds",function(req,res){
 	//get data from form and add to campgrounds arry
 	let name =req.body.name
 	let image = req.body.image
-	let newCampground = {name:name , image:image}
-	campgrounds.push(newCampground)
-	//redirect back to campgrounds
-	res.redirect("/campgrounds");
+	let desc = req.body.description
+	let newCampground = {name:name , image:image, description:desc}
+	//Create a new campground and save tp DB
+	Campground.create(newCampground, function(err,newlyCreated){
+		if(err){
+			console.log(err);
+		}else{
+			//redirect back to campgrounds
+			res.redirect("/campgrounds");		
+		}
+	});
+
 
 });
 
-
+//New - show form to create new campgounds
 app.get("/campgrounds/new", function(req, res){
 	res.render("new")
-})
+});
 	
-
+//SHOW -shows more info about one campground
+app.get("/campgrounds/:id", function(req, res){
+	//Find campgrounds with provided ID
+	Campground.findById(req.params.id,function(err, foundCampground){
+		if(err){
+			console.log(err)
+		}else{
+			//render show templeate with that campground
+			res.render("show",{campground: foundCampground})
+		}
+	});
+	
+});
 
 app.listen(process.env.PORT||3000, process.env.IP, function(){
 	console.log("YelpCamp Has started ")
