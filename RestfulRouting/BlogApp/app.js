7cc0c96fe227665002d03f6a8c06ce8c27f8957a
NisGow <nisha.gowda@utexas.edu>
2020-08-14 07:57:12 +0000

    Created Blog using Rest API

---
+++ RestfulRouting/BlogApp/app.js
@@ -0,0 +1,106 @@
+let express = require("express"),
+	app = express(),
+	methodOverride = require("method-override"),
+	bodyParser = require("body-parser");
+	
+
+
+let mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost:27017/restful_blog_app', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+.then(() => console.log('Connected to DB!'))
+.catch(error => console.log(error.message));
+
+//App config
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(methodOverride("_method"));
+
+//Mongoose /Model Config
+let blogsSchema = new mongoose.Schema({
+	title:String,
+	image: String,
+	body: String,
+	created:{type:Date, default:Date.now}
+	
+});
+
+let Blog = mongoose.model("Blog", blogsSchema);
+
+//Restful Routes
+
+app.get("/",function(req, res){
+	res.redirect("/blogs");
+})
+
+//Index route
+app.get("/blogs",function(req, res){
+	Blog.find({},function(err,blogs){
+		if(err){
+			console.log("error")
+		}else{
+			res.render("index", {blogs:blogs});
+		}
+	})
+	
+})
+
+// new route
+app.get("/blogs/new",function(req, res){
+	res.render("new");
+})
+
+//Create route
+app.post("/blogs",function(req, res){
+	//create blogs
+	Blog.create(req.body.blog, function(err, newBlog){
+		if(err){
+			res.render("new")
+		}else{
+			res.redirect("/blogs");
+		}
+	})
+	//then redirect to the index
+})
+
+//Show route
+app.get("/blogs/:id",function(req, res){
+	Blog.findById(req.params.id, function(err, foundBlog){
+		if(err){
+			res.redirect("/blogs")
+		}else{
+			res.render("show",{blog:foundBlog});
+		}
+	})
+})
+
+//Edit route
+app.get("/blogs/:id/edit",function(req, res){
+	Blog.findById(req.params.id, function(err, foundBlog){
+		if(err){
+			res.redirect("/blogs")
+		}else{
+			res.render("edit",{blog:foundBlog});
+		}
+	})
+})
+	
+	
+//update route
+app.put("/blogs/:id",function(req, res){
+	Blog.findByIdAndUpdate(req.params.id, req.body.blog,function(err,updatedBlog){
+		if(err){
+			res.redirect("/blogs")
+		}else{
+			res.redirect("/blogs/" + req.params.id);
+		}
+	})
+})
+
+
+app.listen(process.env.PORT||3000, process.env.IP, function(){
+	console.log("Server is running")
+})
